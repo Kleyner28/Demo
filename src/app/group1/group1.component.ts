15d82992f9c8d0456b7f2394ec704ca20b674bed
Kleyner <kleineramariz4@gmail.com>
2020-11-24 14:07:49 -0500

    cambios nuevos

--- src/app/group1/group1.component.ts
+++ src/app/group1/group1.component.ts
@@ -5,10 +5,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './group1.component.html',
   styleUrls: ['./group1.component.scss'],
 })
+
+
 export class Group1Component implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
+
+click(){
+var coll = document.getElementsByClassName("collapsible");
+var i=0;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+}
+
 
 }
